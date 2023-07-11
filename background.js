40791eab06714704e4669dc06135b910fa2296ed
Benjamin Denis <benjamin_denis@outlook.com>
2023-07-11 17:35:02 +0200

    fixing didn't work switching to workers in a new branch

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 // Open a connection to the IndexedDB database
-const request = indexedDB.open("HTTPRequestLoggerDB", 1);
 let db;
 
 // Create object store and indexes
+const request = indexedDB.open("HTTPRequestLoggerDB", 1);
+
 request.onupgradeneeded = (event) => {
   db = event.target.result;
 
@@ -25,7 +26,6 @@ request.onupgradeneeded = (event) => {
   });
 };
 
-// Handle database open success
 request.onsuccess = (event) => {
   db = event.target.result;
 };
@@ -65,7 +65,6 @@ function generateResponseFilename(capturedRequest) {
 
 // Function to handle file download when a response filename is clicked
 function handleFileDownload(filename) {
-  // Retrieve the captured request with the corresponding response filename
   const transaction = db.transaction(["requests"], "readonly");
   const objectStore = transaction.objectStore("requests");
   const index = objectStore.index("responseFilename");
@@ -74,115 +73,40 @@ function handleFileDownload(filename) {
   request.onsuccess = (event) => {
     const capturedRequest = event.target.result;
     if (capturedRequest) {
-      // Generate the text content for the response
       const responseText = `Headers:\n${capturedRequest.headers}\n\nPayload:\n${capturedRequest.payload}\n\nResponse:\n${capturedRequest.response}`;
 
-      // Create a Blob with the text content
       const blob = new Blob([responseText], { type: "text/plain" });
 
-      // Create a temporary link element to trigger the download
       const link = document.createElement("a");
       link.href = URL.createObjectURL(blob);
       link.download = filename;
 
-      // Simulate a click on the link to start the download
       link.click();
     }
   };
 }
 
-//... (previously defined code)
-// Initialize an empty array to store captured requests
-let capturedRequests = [];
+// ... (previously defined code)
 
-// Listen for requests using the webRequest API
-chrome.webRequest.onBeforeRequest.addListener(
-	(details) => {
-		// Capture the request details
-		const capturedRequest = {
-			sessionName: '',
-			pageTitle: '',
-			pageUrl: '',
-			requestUrl: details.url,
-			endpointName: '',
-			queryParameterCount: 0,
-			responseSize: 0,
-			responseFilename: ''
-		};
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "startCapture") {
+      capturedRequests = []; // Clear the previous captured requests
+      sendResponse({ success: true });
+    } else if (message.action === "stopCapture") {
+      sendResponse({ success: true, capturedRequests });
 
-		// Retrieve additional information about the request
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			const activeTab = tabs[0];
-			capturedRequest.pageTitle = activeTab.title;
-			capturedRequest.pageUrl = activeTab.url;
-		});
-
-		// Store the captured request in the array
-		capturedRequests.push(capturedRequest);
-	},
-	{ urls: ["<all_urls>"] },
-	["requestHeaders"]
-);
-
-// Listen for completed requests to capture response data
-chrome.webRequest.onCompleted.addListener(
-	(details) => {
-		// Find the captured request in the array and update its response details
-		const capturedRequest = capturedRequests.find(
-			(request) => request.requestUrl === details.url
-		);
-
-		if (capturedRequest) {
-			capturedRequest.responseSize = details.responseHeaders.reduce(
-				(totalSize, header) => {
-					if (header.name.toLowerCase() === "content-length") {
-						return parseInt(header.value);
-					}
-					return totalSize;
-				},
-				0
-			);
-
-			// Generate a filename for the response text file
-			const currentTime = new Date();
-			const timeUnix = Math.floor(currentTime.getTime() / 1000);
-			const timeFriendlyEst = currentTime.toLocaleString("en-US", {
-				timeZone: "America/New_York",
-			});
-			capturedRequest.responseFilename = `${capturedRequest.sessionName}_${capturedRequest.pageTitle}_${capturedRequest.endpointName}_${timeUnix}_${timeFriendlyEst}.txt`;
-		}
-	},
-	{ urls: ["<all_urls>"] },
-	["responseHeaders"]
-);
-
-// Listen for messages from the popup to start or stop session capture
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message.action === "startCapture") {
-		capturedRequests = []; // Clear the previous captured requests
-		sendResponse({ success: true });
-	} else if (message.action === "stopCapture") {
-		sendResponse({ success: true, capturedRequests });
-	}
+      // Store captured requests in IndexedDB
+      storeCapturedRequests();
+    }
+  });
 });
 
-
-// Listen for messages from the popup to start or stop session capture
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "startCapture") {
-    capturedRequests = []; // Clear the previous captured requests
+// Message listener to receive intercepted requests from content script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "capturedRequest") {
+    capturedRequests.push(request.capturedRequest);
     sendResponse({ success: true });
-  } else if (message.action === "stopCapture") {
-    sendResponse({ success: true, capturedRequests });
-
-    // Store captured requests in IndexedDB
-    storeCapturedRequests();
   }
 });
 
-
-// Listen for clicks on response filenames to trigger file download
-chrome.browserAction.onClicked.addListener((tab) => {
-  const filename = tab.url.substring(tab.url.lastIndexOf("/") + 1);
-  handleFileDownload(filename);
-});
